f1d58681de6d68a38499c2a75f2c8a8851d983d3
[sadew1112] <[bbg380027@gmail.com]>
2025-11-03 16:13:46 +0900

    feat : NumberGenerator 클래스 코드 작성
    - 구입한 금액만큼 자동 로또 번호 생성

--- src/main/java/lotto/service/NumberGenerator.java
+++ src/main/java/lotto/service/NumberGenerator.java
@@ -1,4 +1,37 @@
-package lotto.execution;
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+import lotto.util.InputValidator;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberGenerator {
+    private static final int LOTTO_PRICE = 1000;
+    private final OutputView outputView;
+
+    public NumberGenerator(OutputView outputView){
+        this.outputView = outputView;
+    }
+
+    public List<Lotto> getLottos(Integer purchasePrice){
+        int count = purchasePrice / LOTTO_PRICE;
+        outputView.purchaseCount(count);
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++){
+            Lotto lotto = generateLotto();
+            lottos.add(lotto);
+            outputView.lottoNumber(lotto);
+        }
+
+        return lottos;
+    }
+
+    public Lotto generateLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }
