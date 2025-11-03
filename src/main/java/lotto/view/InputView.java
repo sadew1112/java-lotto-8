2bd427c22634471fbc5534a91b8c8a7ec94d45f1
[sadew1112] <[bbg380027@gmail.com]>
2025-11-03 16:17:15 +0900

    feat : InputView 클래스 코드 작성
    - 입력받아야 하는 값들 메서드 분리 및 Consol 호출

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,49 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.InputValidator;
+
+import java.util.List;
+
+public class InputView {
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator){
+        this.inputValidator = inputValidator;
+    }
+    public int inputPurchasePrice() {
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String input = Console.readLine();
+                return inputValidator.priceValidate(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> inputWinningNums() {
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                return inputValidator.winningNumsValidate(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int inputBonusNum(List<Integer> winningNums) {
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                return inputValidator.bonusValidate(input, winningNums);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
