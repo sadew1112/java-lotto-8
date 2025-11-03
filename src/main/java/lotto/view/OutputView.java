c6e43e9f2d4fe158fcde9dc806b8ea30db34856d
[sadew1112] <[bbg380027@gmail.com]>
2025-11-03 16:18:05 +0900

    feat : OutputView 클래스 코드 작성
    - 출력 전용 메서드들 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,39 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+
+import java.math.BigDecimal;
+import java.util.EnumMap;
+import java.util.List;
+
+public class OutputView {
+    public void purchaseCount(Integer count){
+        System.out.println();
+        System.out.printf("%d개를 구매했습니다.%n", count);
+    }
+
+    public void lottoNumber(Lotto lotto){
+        System.out.println(lotto.getNumbers().stream().sorted().toList());
+        System.out.println();
+    }
+
+    public void winningResult(EnumMap<Rank, Integer> result){
+        System.out.println("당첨 통계\n---");
+        System.out.println();
+
+        List<Rank> order = List.of(
+                Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST
+        );
+
+        for (Rank rank : order){
+            int matchCount = result.get(rank);
+            System.out.println(rank.format(matchCount));
+        }
+    }
+
+    public void profitRate(BigDecimal rate) {
+        System.out.printf("총 수익률은 %s%%입니다.%n", rate.toPlainString());
+    }
+
 }
