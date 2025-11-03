12b0a246407d9bf9fcf460187ffd24093ed82785
[sadew1112] <[bbg380027@gmail.com]>
2025-11-03 16:15:43 +0900

    feat : NumberSeparator 클래스 코드 작성
    - 입력 받은 문자열에 대해 쉼표를 구분자로 리스트 변환 로직 구현

--- src/main/java/lotto/util/NumberSeparator.java
+++ src/main/java/lotto/util/NumberSeparator.java
@@ -1,16 +1,56 @@
-package lotto.service;
+package lotto.util;
+
+import lotto.exception.ErrorCode;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class NumberSeparator {
     public List<Integer> separator(String input){
-        return Arrays.stream(input.split(","))
+        List<Integer> numbers = Arrays.stream(input.split(","))
                 .map(String::trim)
-                .filter(num -> !num.isEmpty())
-                .map(num -> Integer.parseInt(num))
-                .distinct()
+                .map(this::parseAndValidate)
                 .collect(Collectors.toList());
+
+        duplicateValidate(numbers);
+        return numbers;
+    }
+
+    private int parseAndValidate(String numStr) {
+        blankValidate(numStr);
+        int num = parseInt(numStr);
+        rangeValidate(num);
+        return num;
+    }
+
+    private void blankValidate(String numStr) {
+        if (numStr.isBlank()) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_INPUT_ERROR.message());
+        }
+    }
+
+    private int parseInt(String numStr) {
+        try {
+            return Integer.parseInt(numStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorCode.IMPOSSIBLE_TRANS_NUM_INPUT.message());
+        }
+    }
+
+    public void rangeValidate(int num) {
+        if (num < 1 || num > 45) {
+            throw new IllegalArgumentException(ErrorCode.OUT_OF_RANGE_NUM.message());
+        }
+    }
+
+    private void duplicateValidate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorCode.DUPLICATE_NUM_INPUT.message());
+        }
     }
 }
+
